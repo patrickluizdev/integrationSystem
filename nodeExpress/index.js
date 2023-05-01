aea8fc33e3cb3ef2a4911202b5dd95fb1fe1f205
patrickluizdev <patrickluizjf@hotmail.com>
2023-05-01 15:07:21 +0000

    nodeContainer

--- nodeExpress/index.js
+++ nodeExpress/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('<body style="background-color:black;   display: flex;justify-content: center; align-items: center;height: 100vh;"> <h1 style="color:green; font-size: 6rem;">Hello Node!</h1></body>');
+  res.send('<!DOCTYPE html><html><head><title>Hello Node!</title><link rel="icon" href="https://www.onemanaged.com/wp-content/uploads/2014/08/world89.png"></head><body style="background-color:black;   display: flex;justify-content: center; align-items: center;height: 100vh;"> <h1 style="color:green; font-size: 6rem;">Hello Node!</h1></body></html>');
 });
 
 app.listen(3000, function () {
